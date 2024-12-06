60ecf766ccc6948db60a184f548a5d6e280b525d
Haodong Wang <99232125+haodong0518@users.noreply.github.com>
2024-12-06 12:04:58 -0600

    Add files via upload

--- serial_communication.ino
+++ serial_communication.ino
@@ -7,6 +7,7 @@ void setup() {
   Serial.begin(9600);
   myservo.attach(9);
   delay(1000);
+  myservo.write(50);
   Serial.println("Type Command (raise, hit)");
 }
 
@@ -15,12 +16,16 @@ void loop() {
     command = Serial.readStringUntil('\n');
     command.trim();
     if (command.equals("raise")) {
-      myservo.write(120);
+      myservo.write(50);
     }
 
     else if (command.equals("hit")) {
-      myservo.write(175);
+      myservo.write(50);
       delay(500);
+      myservo.write(100);
+      delay(500);
+      myservo.write(50);
+      Serial.println("hit finish");
     }
     Serial.print("Command: ");
     Serial.println(command);

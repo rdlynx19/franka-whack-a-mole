2b8dc9b5918dfc25d91056cb8b21f09ca55c665c
rdlynx19 <pushkardave.vnit@gmail.com>
2024-12-10 12:36:59 -0600

    some tweaks, to eliminate redundant movements

--- whack_a_mole/whack_a_mole/camera.py
+++ whack_a_mole/whack_a_mole/camera.py
@@ -14,11 +14,11 @@ from geometry_msgs.msg import TransformStamped
 
 global COLORS, COLORS_HSV, CROP
 
-COLORS = {"GREEN": 0, "YELLOW": 1, "BLUE": 2, "RED": 3}
+COLORS = {"GREEN": 0, "BLUE": 2, "RED": 3}
 
 COLORS_HSV = {
     "GREEN": [np.array([25, 62, 43]), np.array([96, 146, 255])],
-    "YELLOW": [np.array([24, 100, 101]), np.array([24, 184, 173])],
+    # "YELLOW": [np.array([24, 100, 101]), np.array([24, 184, 173])],
     "BLUE": [np.array((103, 104, 83)), np.array((130, 255, 179))],
     "RED": [np.array((2, 201, 85)), np.array((90, 255, 89))],
 }

fdae057cf634baa421540b798b65da09886114b3
haodong <haodong0518@gmail.com>
2024-12-11 11:06:22 -0600

    add arduino codes

---
+++ arduino_codes/servo_serial_communication.ino
@@ -0,0 +1,34 @@
+String command;
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+
+void setup() {
+  Serial.begin(115200);
+  myservo.attach(9);
+  // delay(100);
+  myservo.write(50);
+  // Serial.println("Type Command (raise, hit)");
+}
+
+void loop() {
+  if (Serial.available()) {
+    command = Serial.readStringUntil('\n');
+    command.trim();
+    if (command.equals("r")) {
+      myservo.write(50);
+      Serial.println("d");
+    }
+
+    else if (command.equals("h")) {
+      myservo.write(50);
+      delay(500);
+      myservo.write(100);
+      delay(500);
+      myservo.write(50);
+      Serial.println("d");
+    }
+    // Serial.print("Command: ");
+    // Serial.println(command);
+  }
+}

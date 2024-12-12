09d05f5b9088a6c626109ae4f8ea71d6578a42d2
KhachDavid <khachatryandavid1@gmail.com>
2024-12-11 18:30:03 -0600

    Add press to play and tweak offsets

--- whack_a_mole/whack_a_mole/camera.py
+++ whack_a_mole/whack_a_mole/camera.py
@@ -21,7 +21,7 @@ COLORS_HSV = {
     "GREEN": [np.array([47, 90, 132]), np.array([94, 255, 191])],
     "YELLOW": [np.array([24, 100, 101]), np.array([52, 255, 230])],
     "BLUE": [np.array((95, 157, 85)), np.array((161, 255, 189))],
-    "RED": [np.array((0, 132, 4)), np.array((9, 241, 221))],
+    "RED": [np.array((0, 132, 4)), np.array((2, 241, 221))],
 }
 
 
